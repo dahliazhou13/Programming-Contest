28c4bcb4589bd2d4789cd862553bc9b80543d8e2
zhoudoris13 <zhoudoris13@gmail.com>
2019-12-30 20:00:25 -0500

    Update ThreeDices.java

--- Programming-Contest/src/ThreeDices.java
+++ Programming-Contest/src/ThreeDices.java
@@ -1,3 +1,5 @@
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ThreeDices {
@@ -9,7 +11,12 @@ public class ThreeDices {
 	}
 	public static double SolveForX(double[][] matrix, double[] constants) {
 		double D = determinant(matrix);
-		double[][] xMatrix = matrix;
+		double[][] xMatrix = new double[3][3];
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				xMatrix[i][j]=matrix[i][j];
+			}
+		}
 		xMatrix[0][0]=constants[0];
 		xMatrix[1][0]=constants[1];
 		xMatrix[2][0]=constants[2];
@@ -18,19 +25,29 @@ public class ThreeDices {
 	}
 	public static double SolveForY(double[][] matrix, double[] constants) {
 		double D = determinant(matrix);
-		double[][] yMatrix = matrix;
-		yMatrix[0][0]=constants[0];
-		yMatrix[1][0]=constants[1];
-		yMatrix[2][0]=constants[2];
+		double[][] yMatrix = new double[3][3];
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				yMatrix[i][j]=matrix[i][j];
+			}
+		}
+		yMatrix[0][1]=constants[0];
+		yMatrix[1][1]=constants[1];
+		yMatrix[2][1]=constants[2];
 		double D2 = determinant(yMatrix);
 		return  D2/D;
 	}
 	public static double SolveForZ(double[][] matrix, double[] constants) {
 		double D = determinant(matrix);
-		double[][] zMatrix = matrix;
-		zMatrix[0][0]=constants[0];
-		zMatrix[1][0]=constants[1];
-		zMatrix[2][0]=constants[2];
+		double[][] zMatrix = new double[3][3];
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+				zMatrix[i][j]=matrix[i][j];
+			}
+		}
+		zMatrix[0][2]=constants[0];
+		zMatrix[1][2]=constants[1];
+		zMatrix[2][2]=constants[2];
 		double D3 = determinant(zMatrix);
 		return  D3/D;
 	}
@@ -55,11 +72,26 @@ public class ThreeDices {
 				constants[0] = input.nextDouble()/10000;
 				constants[1] = input.nextDouble()/10000;
 				constants[2] = input.nextDouble()/10000;
+				if(determinant(threeDices)==0) {
+					if(Arrays.equals(threeDices[0],constants)||Arrays.equals(threeDices[1],constants)||Arrays.equals(threeDices[2],constants)) {
+						System.out.println("YES");
+					}
+					else {
+						System.out.println("NO");
+					}
+					continue;
+				}
 				double p1 = SolveForX(threeDices,constants);
 				double p2 = SolveForY(threeDices,constants);
 				double p3 = SolveForZ(threeDices,constants);
-				if((p1>=0 && p2>=0 && p3>=0)&&(p1<=1&&p2<=1&&p3<=1)) {
-					System.out.println("YES");
+				if(p1>=0 && p2>=0 && p3>=0) {
+					if(p1*10000==Math.floor(p1*10000)&&p2*10000==Math.floor(p2*10000)&&p3*10000==Math.floor(p3*10000)) {
+						
+						System.out.println("YES");
+					}
+					else {
+						System.out.println("NO");
+					}
 				}
 				else {
 					System.out.println("NO");
